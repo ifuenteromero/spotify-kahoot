13661abb4ccfd03960de708d5a55c6b838151d3a
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2020-11-11 22:06:37 +0100

    componente login y estilos

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../stylesheets/login.scss';
+import { loginUrl, spotifyLogoUrl } from '../utils/spotify';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='login'>
+      <img src={spotifyLogoUrl} alt='spotify-logo' />
+      <a href={loginUrl}>login with spotify</a>
+    </div>
+  );
 };
 
 export default Login;
