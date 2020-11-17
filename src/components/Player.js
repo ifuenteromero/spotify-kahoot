832bf75eee530a7f21c872d4c9f014c350475161
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2020-11-17 21:12:53 +0100

    refactor some code

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,57 +1,68 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { http } from '../services/httpService';
 import '../stylesheets/player.scss';
+import { getRandomNumber } from '../utils/useFulFunctions';
+import endPoints, { tracksFromPlayList } from '../utils/spotifyUrls';
 
 const Player = () => {
   const playerRef = useRef();
+  const { userPlayLists } = endPoints;
   useEffect(() => {
     getPlayLists();
   }, []);
+
   const [tracks, setTracks] = useState([]);
+
   useEffect(() => {
     getRandomTrack();
   }, [tracks]);
 
-  const getRandomArbitrary = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
+  const pauseTrack = () => {
+    const trackDuration = 5000;
+    setTimeout(() => {
+      playerRef.current.pause();
+    }, trackDuration);
   };
 
   const getRandomTrack = () => {
     const totalTracks = tracks.length;
-    const randomNumber = getRandomArbitrary(0, totalTracks - 1);
+    const randomNumber = getRandomNumber(0, totalTracks - 1);
     setRandomTrack(tracks[randomNumber]);
-    setTimeout(() => {
-      playerRef.current.pause();
-    }, 5000);
+    pauseTrack();
   };
+
   const [randomTrack, setRandomTrack] = useState({
     id: null,
     preview_url: null
   });
+
   const getPlayLists = async () => {
     const {
       data: { items: playLists }
-    } = await http.get('/me/playlists');
-    console.log({ playLists });
-    const resultado = await Promise.all(
-      playLists.map(async playList =>
-        http.get(`/playlists/${playList.id}/tracks`)
-      )
+    } = await http.get(userPlayLists);
+
+    const playListsTracksResponse = await Promise.all(
+      playLists.map(async playList => http.get(tracksFromPlayList(playList.id)))
     );
 
-    console.log({ resultado });
-    let _tracks = [];
-    resultado.forEach(
-      item =>
-        (_tracks = [..._tracks, ...item.data.items.map(({ track }) => track)])
+    const _tracks = playListsTracksResponse.reduce(
+      (totalTracks, trackResponse) => {
+        const playListTrackList = trackResponse.data.items
+          .map(({ track }) => track)
+          .filter(track => track.preview_url);
+        totalTracks = [...totalTracks, ...playListTrackList];
+        return totalTracks;
+      },
+      []
     );
-    setTracks(_tracks.filter(track => track.preview_url));
+    setTracks(_tracks);
   };
+
   return (
     <>
-      {randomTrack?.preview_url && (
-        <audio controls ref={playerRef} className='player'>
-          <source src={randomTrack?.preview_url} type='audio/mpeg' />
+      {randomTrack && (
+        <audio controls ref={playerRef} autoPlay className='player'>
+          <source src={randomTrack.preview_url} type='audio/mpeg' />
         </audio>
       )}
     </>
--- src/utils/spotifyUrls.js
+++ src/utils/spotifyUrls.js
@@ -1,5 +1,9 @@
 const currentUserProfileEndPoint = '/me';
+const userPlayLists = '/me/playlists';
+export const tracksFromPlayList = playListId =>
+  `/playlists/${playListId}/tracks`;
 
 export default {
-  currentUserProfileEndPoint
+  currentUserProfileEndPoint,
+  userPlayLists
 };
